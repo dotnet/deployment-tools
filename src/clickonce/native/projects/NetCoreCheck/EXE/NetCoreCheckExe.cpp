2366d3eb1aec4db082c6905811314db54d0c4726
Luke Westendorf <lukewest@microsoft.com>
2021-01-26 16:39:10 -0800

    Updating command line parameters and adding help text

--- src/clickonce/native/projects/NetCoreCheck/EXE/NetCoreCheckExe.cpp
+++ src/clickonce/native/projects/NetCoreCheck/EXE/NetCoreCheckExe.cpp
@@ -4,49 +4,77 @@
 
 #include "NetCoreCheckExe.h"
 
+#define PARAM_NAME(s) (_wcsicmp(*argv, TEXT(s)) == 0)
+
 // Globals
 Logger *g_log;
+const char* g_help = "NETCoreCheck [options]\n"
+"-n, --runtimename -       Runtime name                    (Example: Microsoft.AspNetCore.App)\n"
+"-v, --runtimeversion -    Runtime version in format x.y.z (Example: 1.2.3)\n"
+"-r, --rollforwardpolicy - (Optional) Roll forward policy  (Example: LatestMajor)\n"
+"-c, --runtimeconfigfile - Path to runtime config file     (Example: c:\\Foo\\Bar.runtimeconfig.json)\n"
+"-l, --logfile -           (Optional) Path to log file\n\n"
+"If runtimeconfigfile is specified then runtimename, runtimeversion and rollforwardpolicy shouldn't be.\n\n"
+"Examples:\n\n"
+"NETCorecheck --runtimename Microsoft.AspNetCore.App --runtimeversion 3.1.0\n"
+"NETCorecheck -n Microsoft.WindowsDesktop.App -v 5.0.1 -r LatestMajor\n"
+"NETCorecheck -c c:\\Foo\\Bar.runtimeconfig.json -l c:\\Foo\\Bar.log\n";
 
 int __cdecl wmain(int argc, WCHAR* argv[])
 {
-    if (argc < 3 || argc > 5)
+    LPCWSTR runtimeName = NULL;
+    LPCWSTR runtimeversion = NULL;
+    LPCWSTR rollForwardPolicy = NULL;
+    LPCWSTR existingRuntimeConfigFilPath = NULL;
+    LPCWSTR logFilePath = NULL;
+
+    // Parse the command line options
+    argv++;
+    for (int i = 1; i < argc; i++)
     {
-        return EXIT_FAILURE_INVALIDARGS;
+        if (*argv != nullptr)
+        {
+            if (PARAM_NAME("-n") || PARAM_NAME("--runtimename"))
+            {
+                runtimeName = *++argv;
+            }
+            else if (PARAM_NAME("-v") || PARAM_NAME("--runtimeversion"))
+            {
+                runtimeversion = *++argv;
+            }
+            else if (PARAM_NAME("-r") || PARAM_NAME("--rollforwardpolicy"))
+            {
+                rollForwardPolicy = *++argv;
+            }
+            else if (PARAM_NAME("-c") || PARAM_NAME("--runtimeconfigfile"))
+            {
+                existingRuntimeConfigFilPath = *++argv;
+            }
+            else if (PARAM_NAME("-l") || PARAM_NAME("--logfile"))
+            {
+                logFilePath = *++argv;
+            }
+            else
+            {
+                fprintf(stderr, "%s", g_help);
+                return EXIT_FAILURE_INVALIDARGS;
+            }
+
+            argv++;
+        }
     }
 
-    bool useExistingFile = (0 == _wcsicmp(TEXT("UseExisting"), argv[1]));
-    LPCWSTR logFilePath = useExistingFile ?
-        ((argc == 4) ? argv[3] : NULL) :
-        ((argc == 5) ? argv[4] : NULL);
+    // Check for invalid parameter combinations
+    if ((existingRuntimeConfigFilPath  &&  (runtimeName || runtimeversion || rollForwardPolicy)) ||
+        (!existingRuntimeConfigFilPath && !(runtimeName && runtimeversion)))
+    {
+        fprintf(stderr, "%s", g_help);
+        return EXIT_FAILURE_INVALIDARGS;
+    }
 
     FileLogger logger;
     logger.Initialize(logFilePath);
     g_log = &logger;
 
-    // There's two valid sets of parameters.
-    //  1. If the first parameter is 'UseExisting' the second parameter needs to be the path to a
-    //     runtimeconfig.json file that we'll try to load.
-    //     Example: NetCoreCheck.exe UseExisting Foo.runtimeconfig.json
-    //  2. Otherwise we'll create a temporary runtime config file ourselves using the passed
-    //     in framework name and framework version, and optionally the roll forward policy.
-    //     Examples: NetCoreCheck.exe Microsoft.WindowsDesktop.App 3.1.0
-    //               NetCoreCheck.exe Microsoft.NETCore.App 3.1.0 LatestMajor
-    // In both cases the optional last parameter is the path to the log file.
-    int result;
-    if (useExistingFile)
-    {
-        LPCWSTR existingRuntimeConfigFile = argv[2];
-
-        result = CheckRuntime(NULL, NULL, NULL, existingRuntimeConfigFile, false);
-    }
-    else
-    {
-        LPCWSTR frameworkName = argv[1];
-        LPCWSTR frameworkVersion = argv[2];
-        LPCWSTR rollForwardPolicy = argc > 3 ? argv[3] : NULL;
-
-        result = CheckRuntime(frameworkName, frameworkVersion, rollForwardPolicy, NULL, false);
-    }
-
-    return result;
+    return CheckRuntime(runtimeName, runtimeversion, rollForwardPolicy, existingRuntimeConfigFilPath, false);
 }
